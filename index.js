50d558bcee12d1eb87236b6a28b1fa40a6748e2b
Herno <114448348+Herno89@users.noreply.github.com>
2025-10-24 02:13:51 -0300

    Get products con await

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("HOla Mundito");
+//console.log(process.argv);
+//const method = process.argv[2];
+//const resource = process.argv[3];
+
+//console.log(method, resource);
+let [, , method, resource]= process.argv;
+
+method= method.toUpperCase();
+resource= resource.toLowerCase();
+
+if (method == "GET"&& resource == "products"){
+   const response = await  fetch('https://fakestoreapi.com/products');
+   const data = await response.json();
+   console.log(data);
+    
+}
