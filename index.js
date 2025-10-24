4cf685499f5394321b9b6fbc0aa2aeedebd8aae5
Herno <114448348+Herno89@users.noreply.github.com>
2025-10-24 02:46:45 -0300

    GET products (id)

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ let [, , method, resource]= process.argv;
 method= method.toUpperCase();
 resource= resource.toLowerCase();
 
+if (method=="GET"&& resource.startsWith("products/")){
+    //const params = resource.split("/");
+    let id = resource.split("/")[1];
+    id = parseInt(id);
+    if (isNaN(id)|| id<=0){
+        console.log("No es un numero");
+    }    
+    fetch('https://fakestoreapi.com/products/'+id)
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+
+
 if (method == "GET"&& resource == "products"){
    const response = await  fetch('https://fakestoreapi.com/products');
    const data = await response.json();
