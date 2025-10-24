e7aab171e86ddca6108eaf01f13c5b7978aa724d
Herno <114448348+Herno89@users.noreply.github.com>
2025-10-24 05:29:34 -0300

    DELETE

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let [, , method, resource, ...params]= process.argv;
 method= method.toUpperCase();
 resource= resource.toLowerCase();
 
-if (method= "POST" && resource == "products"){
+if (method== "POST" && resource == "products"){
 
     const [title, price, category] = params;
 
@@ -27,6 +27,41 @@ if (method= "POST" && resource == "products"){
     .then(response => response.json())
     .then(data => console.log(data));
 }
+// Modificar
+if (method == "PUT"&& resource.startsWith("products/")){
+    
+    let id = resource.split("/")[1];
+    id = parseInt(id);   
+    const [title, price, category] = params;
+
+
+    const product = { 
+        title, 
+        price,
+        category
+     };
+
+    fetch('https://fakestoreapi.com/products/' + id, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(product)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+}
+
+//Eliminar
+if (method == "DELETE" && resource.startsWith("products/")){
+
+    const id = parseInt(resource.split("/")[1]);
+
+    fetch('https://fakestoreapi.com/products/'+id, {
+  method: 'DELETE'
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
 
 
 if (method=="GET"&& resource.startsWith("products/")){
